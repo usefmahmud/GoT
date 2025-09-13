d0d19b84aeb37a4285a44e4746b7a870e158af8c
usefmahmud <usef.mahmoud@outlook.com>
2025-09-13 21:05:27 +0300

    feat: add optional directory argument to init command

--- cmd/init.go
+++ cmd/init.go
@@ -9,8 +9,9 @@ import (
 )
 
 var initCommand = &cobra.Command{
-	Use: "init",
+	Use: "init [directory]",
 	Short: "initialize your repository",
+	Args: cobra.MaximumNArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
 		wd, err := os.Getwd()
 		if err != nil {
@@ -18,11 +19,25 @@ var initCommand = &cobra.Command{
 			return err
 		}
 
+		// if user give a folder name "git init folder-name", so we use it as root directory
+		if( len(args) > 0) {
+			wd = filepath.Join(wd, args[0])
+
+			if _, err := os.Stat(wd); os.IsNotExist(err) {
+				if MkdirErr := os.Mkdir(wd, 0755); MkdirErr != nil {
+					return fmt.Errorf("failed to create directory %s", args[0])
+				}
+			}
+		}
+		
+
 		gotDir := filepath.Join(wd, ".got")
-    err = os.Mkdir(gotDir, 0755)
-		if err != nil {
-			fmt.Println("Failed to initialize the repository")
+		if _, err :=  os.Stat(gotDir); err == nil {
 			return nil
+		}
+
+		if err := os.Mkdir(gotDir, 0755); err != nil {
+			return fmt.Errorf("failed to initialize the repository")
 		}
 
 		return nil
